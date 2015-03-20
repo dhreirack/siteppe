830c44de2f9a108ea677e96422ac35708e9bf8d3
Dhrei portable <dhreirack@gmail.com>
2015-03-20 10:48:27 +0100

    changement db

--- siteadmin/pdo/MyPdo.php
+++ siteadmin/pdo/MyPdo.php
@@ -6,7 +6,7 @@ class MyPdo
 	public function __construct(){
 		try
 		{
-			$this->connection=new PDO('mysql:host=localhost;dbname=si6;charset=utf8','root','');
+			$this->connection=new PDO('mysql:host=localhost;dbname=site admin;charset=utf8','root','');
 			//permet de récup les erreurs
 			$this->connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
                        
