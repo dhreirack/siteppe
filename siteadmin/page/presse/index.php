038a4bd89b1ac2da348fc641fe6a3bf13ef5a682
sidahmedrami <sideramidu39@msn.com>
2015-04-03 10:42:44 +0200

    modif presse

--- siteadmin/page/presse/index.php
+++ siteadmin/page/presse/index.php
@@ -1,10 +1,10 @@
 <div class="row">
     <div class="col-lg-12">
         <h1 class="page-header">
-          //  Presse \\ <small> Gérer les articles</small>
+          //  Presse \\ <small> Gérer la presse</small>
         </h1>
 
-<a class="navbar-brand" href="index.php?controller=presse&action=create">Créer un article</a>
+<a class="navbar-brand" href="index.php?controller=presse&action=create">Créer un article de presse/a>
 
         <table class="table table-striped">
             <thead>
