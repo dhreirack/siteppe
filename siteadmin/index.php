485edff1cf3f7fbe3ce7021784e7c90f71ac41f8
Dhrei portable <dhreirack@gmail.com>
2015-04-09 08:57:10 +0200

    rename

--- siteadmin/index.php
+++ siteadmin/index.php
@@ -10,7 +10,7 @@
     <meta name="description" content="">
     <meta name="author" content="Groupe Bonvalot">
 
-    <title>Site SI6</title>
+    <title>Site PPE</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="public/css/bootstrap.min.css" rel="stylesheet">
