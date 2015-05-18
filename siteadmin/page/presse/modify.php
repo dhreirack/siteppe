95deb0527319b243266bc0b7e56d3295b2cbb9ff
Thomas <t.bonvalot@outlook.fr>
2015-05-18 15:47:52 +0200

    modifier
    La fonction modifier fonctionne

--- siteadmin/page/presse/modify.php
+++ siteadmin/page/presse/modify.php
@@ -1,8 +1,10 @@
-<form action="index.php?controller=presse&action=modify" method="post">
-	titre : <input type="text" name="nomarticle">
-	description : <input type="text" name="descriptionarticle">
-	corps : <input type="fulltext" name="corpsarticle">
-
-
-	<input type="submit">
+Bienvenue sur modifier une nouvelle
+<form action="index.php?action=update&id=<?php echo $lanews->id ; ?>" enctype="multipart/form-data" method="post">
+	Titre : <input type="text" name="titre" value="<?php echo $lanews->titre ; ?>">
+	Description : <input type="text" name="description"  value="<?php echo $lanews->description ; ?>">
+	<!-- MAX_FILE_SIZE doit précéder le champ input de type file -->
+  <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+  <!-- Le nom de l'élément input détermine le nom dans le tableau $_FILES -->
+  Envoyez ce fichier : <input name="userfile" type="file" />
+  	<input type="submit">
 </form>
