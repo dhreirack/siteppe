4e8c670a508eb348cc0c57d26c34b185b173a2fe
Acquistapace <dark-moon_1412@hotmail.fr>
2015-05-18 16:00:19 +0200

    Intégration de l'outils d'édition dans Histoire

--- siteadmin/page/histoire/create.php
+++ siteadmin/page/histoire/create.php
@@ -1,8 +1,7 @@
 <form action="index.php?controller=histoire&action=store" method="post">
 	Nom : <input type="text" name="nomhistoire">
 	<br></br>
-	Contenu : <br></br>
-	<textarea id="descriptionarticle"  name="contenuhistoire"></textarea>
+	Contenu : <textarea id="corpsarticle"  name="contenuhistoire"></textarea> <!-- L'ID reste corps pour le CSS (la mise en page)-->
 	<br></br>
 
 	<input type="submit">
